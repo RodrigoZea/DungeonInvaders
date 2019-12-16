bb3aadd464cc5aa7621ff7fd46abc9c93d0ff57c
Rodrigo Zea <zearodrigo37@gmail.com>
2019-12-15 22:47:19 -0600

    Subiendo proyecto base

---
+++ DungeonInvaders/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D prb;
+    private Vector2 movSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += mov * Time.deltaTime * speed;
+    }
+}
