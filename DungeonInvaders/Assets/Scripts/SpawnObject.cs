2b2de9222ea11facbffa7b3cfeea38db1d7f94e2
josepablito16 <josepablocif16@gmail.com>
2019-12-17 07:25:46 -0600

    Creación de sala individual LR, LRT, LRTB

---
+++ DungeonInvaders/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject[] objects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0,objects.Length);
+        Instantiate(objects[rand],transform.position,Quaternion.identity);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
