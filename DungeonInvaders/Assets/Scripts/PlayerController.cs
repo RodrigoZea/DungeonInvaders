8f62c443772438c9b80fc241fa2b2e024bf507b9
Rodrigo Zea <zearodrigo37@gmail.com>
2019-12-16 23:33:52 -0600

    salto basico, aun son infinitos debido a que se desconoce la implementacion del mapa

--- DungeonInvaders/Assets/Scripts/PlayerController.cs
+++ DungeonInvaders/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float jumpForce = 5f;
     private Rigidbody2D prb;
     private Vector2 movSpeed;
 
@@ -17,7 +18,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Jump();
         Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += mov * Time.deltaTime * speed;
+    }
+
+    void Jump() {
+        if (Input.GetButtonDown("Jump")) {
+            prb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
     }
 }
