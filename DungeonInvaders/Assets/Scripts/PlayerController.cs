3cc0fad14ae84c1d2914284948b620aef1085cbd
Rodrigo Zea <zearodrigo37@gmail.com>
2019-12-17 00:09:23 -0600

    raycast implementacion para salto

--- DungeonInvaders/Assets/Scripts/PlayerController.cs
+++ DungeonInvaders/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private LayerMask platformLayerMask;
     public float speed;
     public float jumpForce = 5f;
     private Rigidbody2D prb;
     private Vector2 movSpeed;
+    private BoxCollider2D boxCollider2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        prb = GetComponent<Rigidbody2D>();    
+        prb = GetComponent<Rigidbody2D>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -24,8 +27,26 @@ public class PlayerController : MonoBehaviour
     }
 
     void Jump() {
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
             prb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
+    }
+
+    bool IsGrounded() {
+       float extraHeight = 0.5f;
+       RaycastHit2D raycastHit = Physics2D.Raycast(boxCollider2d.bounds.center, Vector2.down, boxCollider2d.bounds.extents.y + extraHeight, platformLayerMask);
+
+        Color rayColor;
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else {
+            rayColor = Color.red;
+        }
+
+        Debug.DrawRay(boxCollider2d.bounds.center, Vector2.down * (boxCollider2d.bounds.extents.y + extraHeight), rayColor);
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
     }
 }
