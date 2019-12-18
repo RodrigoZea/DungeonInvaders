c115b2e3d3be093dd01a4c25d613addd29d5d2c0
josepablito16 <josepablocif16@gmail.com>
2019-12-17 19:26:50 -0600

    Se crean salas random infinitamente, hacen falta un par de reglas para construir el path correctamente

---
+++ DungeonInvaders/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform[] startingPositions;
+    public GameObject[] rooms;
+    public float moveAmount;
+
+    private int direction;
+    private float timeBtwRoom;
+    public float startTimeBtwRoom=0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randStartingPos = Random.Range(0, startingPositions.Length);
+        transform.position = startingPositions[randStartingPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwRoom <= 0)
+        {
+            Move();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else
+        {
+            timeBtwRoom -= Time.deltaTime;
+        }
+
+
+    }
+
+    private void Move()
+    {
+        if (direction == 1 || direction == 2)
+        {
+            //MOVE RIGHT
+            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+            transform.position = newPos;
+        }
+        else if (direction == 3 || direction == 4)
+        {
+            //MOVE LEFT
+            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+            transform.position = newPos;
+        }
+        else if (direction == 5)
+        {
+            //MOVE DOWN
+            Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+            transform.position = newPos;
+        }
+
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+        direction = Random.Range(1, 6);
+    }
+}
